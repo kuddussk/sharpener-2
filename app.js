58f781a2d582321bc1e8e8c691e62cd4ff972fc5
kuddussk <kuddussk19506@gmail.com>
2023-09-13 04:03:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//By claa name
+
+/*let items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor='green';
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}*/
+
+//by tag name
+let li=document.getElementsByTagName('li')
+console.log(li);
+li[2].style.backgroundColor='green';
+for(let i=0;i<li.length;i++)
+{
+    li[i].style.fontWeight='bold';
+}
